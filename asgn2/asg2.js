a87170d0c7ea4c90c1ab770cb7012f6618793e82
Kiaran Lau <kiaranlau@MacBook-Pro-3.local>
2025-04-28 03:11:23 -0700

    mouse differentials + moving eyes

--- asgn2/asg2.js
+++ asgn2/asg2.js
@@ -86,6 +86,7 @@ function connectVariablesToGLSL(){
 
 
 // Globals related to UI Elements
+var g_animationBoolean = true;
 var g_selectedColor = [1.0, 1.0, 1.0, 1.0];
 var g_segments = 6;
 var g_globalAngle = 0;
@@ -95,11 +96,17 @@ var g_neckAngle = 0;
 var g_torsoAngle = 0;
 var g_beakAngle = 0;
 
+var g_mouseCoords = {x:0, y:0};
+
+
+
 function addActionsForHtmlUI(){
     // Button Events
-    document.getElementById('clearButton').onclick = function(){
-        g_shapesList = [];
-        gl.clear(gl.COLOR_BUFFER_BIT);
+    animationButton = document.getElementById('animationButton');
+    animationButton.onclick = function(){
+        g_animationBoolean = !g_animationBoolean;
+        // if (g_animationBoolean) {g_startTime = performance.now()/1000.0; }
+        g_animationBoolean ? animationButton.style.backgroundColor = 'rgba(0.0, 220.0, 50.0, 1.0)' : animationButton.style.backgroundColor = 'rgba(240.0, 0.0, 20.0, 1.0)';
     };
 
     // Angle Slider
@@ -125,6 +132,9 @@ function addActionsForHtmlUI(){
     // Beak Slider
     var beakSlide = document.getElementById('beakSlide');
     beakSlide.addEventListener('input', function() { g_beakAngle = this.value; renderAllShapes(); });
+
+    // Canvas Mouse Move
+    canvas.addEventListener('mousemove', handleMouseMove);
 }
 
 function sendPerformanceStatsToHTML(text, id){
@@ -145,7 +155,8 @@ function main() {
     // canvas.onmousedown = click;
     // canvas.onmousemove = function(ev){ if (ev.buttons == 1){ click(ev); } };
 
-    renderAllShapes();
+    // renderAllShapes();
+    requestAnimationFrame(tick);
 }
 
 
@@ -196,6 +207,8 @@ function renderAllShapes(){
     // Penguin Builder
     // Body Cube
 
+    var targetAngle;
+
     var bodyx = 0.5;
     var bodyy = 1.0;
     var bodyz = bodyx;
@@ -204,7 +217,11 @@ function renderAllShapes(){
     
     bodyMatrix.setTranslate(-.25, -.5, -.25);
     bodyMatrix.translate(bodyx/2, bodyy/2, bodyz/2);
-    bodyMatrix.rotate(g_torsoAngle, 1, -1, 0);
+    targetAngle = g_animationBoolean ? 30*Math.sin(g_seconds) : g_torsoAngle;
+    var bodyRotAxisX = g_animationBoolean ? 1*Math.sin(g_seconds/4) : 1;
+    var bodyRotAxisY = g_animationBoolean ? 3*Math.cos(g_seconds/7) : -1; 
+    // bodyMatrix.rotate(, 1, -1, 0);
+    bodyMatrix.rotate(targetAngle, bodyRotAxisX, bodyRotAxisY, 0);
     bodyMatrix.translate(-bodyx/2, -bodyy/2, -bodyz/2);
     bodyMatrix.scale(bodyx, bodyy, bodyz); // Scale happens first (?) according to 2.2
     drawCube(bodyMatrix, bodyColor);
@@ -216,9 +233,10 @@ function renderAllShapes(){
     
     headColor = bodyColor;
     // headColor = green;
+    targetAngle = g_animationBoolean ? 20*Math.sin(g_seconds*2/3) : -g_neckAngle;
     headMatrix.translate(-0.3/2+0.5/2, 1, -0.3/2);
     headMatrix.translate(hlc, hlc_y, hlc);
-    headMatrix.rotate(-g_neckAngle, 1, 0, 0);
+    headMatrix.rotate(targetAngle, 1, 0, 0);
     headMatrix.translate(-hlc, -hlc_y, -hlc);
     headMatrix.scale(0.3, 0.3, 0.3); 
     drawCube(headMatrix, headColor);
@@ -227,16 +245,18 @@ function renderAllShapes(){
     var rfin_mx = createChildMatrixOfParent(bodyMatrix, bodyx, bodyy, bodyz);
     fin_color = bodyColor;
     // fin_color = [0.2, .5, 1.0, 1.0];
-    rfin_mx.translate(0.5, 0.9, 0.25);
-    rfin_mx.rotate(g_finAngle, 0, 0, 1);
+    rfin_mx.translate(0.5, 0.9, 0.25+0.15);
+    targetAngle = g_animationBoolean ? -20*Math.sin(g_seconds*2)+20 : g_finAngle;
+    rfin_mx.rotate(targetAngle, 0, 0, 1);
     rfin_mx.rotate(180, 1, 0, 0);
     rfin_mx.scale(0.1, 0.8, 0.25);
     drawCube(rfin_mx, fin_color);
 
     // Left fin
     var lfin_mx = createChildMatrixOfParent(bodyMatrix, bodyx, bodyy, bodyz);
-    lfin_mx.translate(0, 0.9, 0.25);
-    lfin_mx.rotate(-g_finAngle, 0, 0, 1);
+    lfin_mx.translate(0, 0.9, 0.25/2);
+    targetAngle = g_animationBoolean ? 20*Math.sin(g_seconds*2)-20 : -g_finAngle;
+    lfin_mx.rotate(targetAngle, 0, 0, 1);
     lfin_mx.rotate(180, 0, 0, 1);
     lfin_mx.scale(0.1, 0.8, 0.25);
     drawCube(lfin_mx, fin_color);
@@ -249,14 +269,16 @@ function renderAllShapes(){
 
     // Right foot
     var rfoot = createChildMatrixOfParent(bodyMatrix, bodyx, bodyy, bodyz);
+    targetAngle = g_animationBoolean ? 45*Math.sin(g_seconds*4) : g_footAngle; 
     rfoot.translate(0.3+0.1/2, -0.2/2, -0.3/2);
-    rotateLocalCenter(rfoot, g_footAngle, flc_x/2, flc_y/2, flc_z*.8)
+    rotateLocalCenter(rfoot, targetAngle, flc_x/2, flc_y/2, flc_z*.8);
     rfoot.scale(0.2, 0.1, 0.3); // first
     
     // Left Foot
     var lfoot = createChildMatrixOfParent(bodyMatrix, bodyx, bodyy, bodyz);
+    targetAngle = g_animationBoolean ? -45*Math.sin(g_seconds*4) : -g_footAngle;
     lfoot.translate(-.1/2, -0.2/2, -0.3/2);
-    rotateLocalCenter(lfoot, -g_footAngle, flc_x/2, flc_y/2, flc_z*.8)
+    rotateLocalCenter(lfoot, targetAngle, flc_x/2, flc_y/2, flc_z*.8);
     lfoot.scale(0.2, 0.1, 0.3);
     
     drawCube(rfoot, orange);
@@ -275,15 +297,17 @@ function renderAllShapes(){
     var beakz = 0.1;
 
     var upperBeak = createChildMatrixOfParent(headMatrix, 0.3, 0.3, 0.3);
+    targetAngle = g_animationBoolean ? 5*Math.sin(g_seconds*3/2) + 5*Math.cos(g_seconds/3) + 10 : g_beakAngle;
     upperBeak.translate(0.1, 0.05, -0.1);
-    rotateLocalCenter(upperBeak, g_beakAngle, beakx/2, beaky/2, beakz);
+    rotateLocalCenter(upperBeak, targetAngle, beakx/2, beaky/2, beakz);
     upperBeak.scale(beakx, beaky, beakz);
     drawCube(upperBeak, orange);
 
     // Lower Beak
     var lowerBeak = createChildMatrixOfParent(headMatrix, 0.3, 0.3, 0.3);
+    targetAngle = -targetAngle
     lowerBeak.translate(0.1, 0.05-beaky/2, -0.1);
-    rotateLocalCenter(lowerBeak, -g_beakAngle, beakx/2, beaky/2, beakz);
+    rotateLocalCenter(lowerBeak, targetAngle, beakx/2, beaky/2, beakz);
     lowerBeak.scale(beakx, beaky, beakz);
     drawCube(lowerBeak, orange);
 
@@ -304,14 +328,18 @@ function renderAllShapes(){
     drawCube(rightEye, lightwhite);
 
     var leftPupil = createChildMatrixOfParent(leftEye, eyex, eyey, eyez);
-    leftPupil.translate(eyex*.35, 0, -eyez*.5);
+    var pmd = calculateMouseToPupilDifferential(leftPupil.elements[12], leftPupil.elements[13]); // pmd = pupilMouseDiff
+    leftPupil.translate(eyex*.25+pmd.xd*0.02, eyey*0.15+pmd.yd*0.015, -eyez*.5);
     leftPupil.scale(eyex*.5, eyey*.8, eyez*.5);
     drawCube(leftPupil, black);
 
     var rightPupil = createChildMatrixOfParent(rightEye, eyex, eyey, eyez);
-    rightPupil.translate(eyex*0.17, 0, -eyez*.5);
+    var pmd = calculateMouseToPupilDifferential(rightPupil.elements[12], rightPupil.elements[13]); // pmd = pupilMouseDiff
+    rightPupil.translate(eyex*0.25+pmd.xd*0.02, eyey*0.15+pmd.yd*0.015, -eyez*.5);
     rightPupil.scale(eyex*.5, eyey*.8, eyez*.5);
     drawCube(rightPupil, black);
+
+    // console.log("rightPupil coords:" + rightPupil.elements[12], rightPupil.elements[13], rightPupil.elements[14] + "\nmouseCoords: " + g_mouseCoords.x + ", " + g_mouseCoords.y);
 
     let duration = performance.now() - startTime;
     sendPerformanceStatsToHTML("ping (ms): " + Math.floor(duration) + " | fps: " + Math.floor(10000/duration)/10, "numdot");
@@ -330,9 +358,38 @@ var g_seconds = performance.now()/1000.0 - g_startTime;
 function tick(){
     // global tick variable update
     g_seconds=performance.now()/1000.0 - g_startTime;
-    console.log(g_seconds);
+    // console.log(g_seconds);
 
     renderAllShapes();
 
     requestAnimationFrame(tick);
+}
+
+function handleMouseMove(event){
+    g_mouseCoords = convertToWebGLCoords(event);
+
+    // console.log(g_mouseCoords);
+}
+
+function convertToWebGLCoords(event){
+    const rect = canvas.getBoundingClientRect(); // Get canvas position and size
+
+    // Calculate mouse position relative to the canvas's top-left corner
+    const pixelX = event.clientX - rect.left;
+    const pixelY = event.clientY - rect.top;
+
+    // Convert pixel coordinates to WebGL clip space coordinates
+    const clipX = (pixelX / canvas.width) * 2 - 1;
+    // Flip Y axis: (pixelY / canvas.height) gives 0 at top, 1 at bottom
+    // We want 1 at top, -1 at bottom
+    const clipY = 1 - (pixelY / canvas.height) * 2;
+
+    return { x: clipX, y: clipY };
+}
+
+function calculateMouseToPupilDifferential(worldposX, worldposY){
+    var xdiff = g_mouseCoords.x - worldposX;
+    var ydiff = g_mouseCoords.y - worldposY;
+
+    return {xd: Math.max(-1, Math.min(xdiff*3, 1)), yd: Math.max(-1, Math.min(ydiff*3, 1))};
 }
