01185c8585dcf44bd363cf6b6481801e40717644
Kiaran Lau <kiaranlau@MacBook-Pro-5.local>
2025-05-11 23:02:11 -0700

    still fixing asgn2 for a projection and view matrix

---
+++ asgn3/Cube.js
@@ -0,0 +1,50 @@
+class Cube{
+    constructor(){
+        this.type='Cube';
+        this.color=g_selectedColor.slice();
+        this.color=[1.0, 1.0, 1.0, 1.0];
+        this.matrix = new Matrix4();
+    }
+    render(){
+        var rgba = this.color;
+
+        this.matrix.setIdentity();
+        gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+        gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0);
+        drawTriangle3D([0, 0, 0,  1, 0, 0,  0, 1, 0]); // flat triangle on z = 0
+
+        // Pass the matrix to u_ModelMatrix attribute
+        gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+        // Pass the color of a point to u_FragColor variable
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+        
+        // Front Face z = 0.0
+        drawTriangle3D( [0.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0 ]);
+        drawTriangle3D( [0.0,0.0,0.0, 1.0,0.0,0.0, 1.0,1.0,0.0 ]);
+        // Front: new 3d UV triangle test
+        // drawTriangle3DUV([0,0,0, 1,1,0, 1,0,0], [1,0, 0,1, 1,1]); 
+
+        // Top Face y = 1.0
+        drawTriangle3D( [0.0,1.0,0.0, 0.0,1.0,1.0, 1.0,1.0,1.0 ]);
+        drawTriangle3D( [0.0,1.0,0.0, 1.0,1.0,0.0, 1.0,1.0,1.0 ]);
+
+        // Left Face x = 0.0
+        drawTriangle3D( [0.0,0.0,0.0, 0.0,1.0,0.0, 0.0,1.0,1.0 ]);
+        drawTriangle3D( [0.0,0.0,0.0, 0.0,0.0,1.0, 0.0,1.0,1.0 ]);
+
+        // Pass the color of a point to u_FragColor variable
+        gl.uniform4f(u_FragColor, rgba[0]*0.9, rgba[1]*0.9, rgba[2]*0.9, rgba[3]*1);
+
+        // Bottom Face y = 0.0
+        drawTriangle3D( [0.0,0.0,0.0, 0.0,0.0,1.0, 1.0,0.0,1.0 ]);
+        drawTriangle3D( [0.0,0.0,0.0, 1.0,0.0,0.0, 1.0,0.0,1.0 ]);
+
+        // Right Face x = 1.0
+        drawTriangle3D( [1.0,0.0,0.0, 1.0,1.0,0.0, 1.0,1.0,1.0 ]);
+        drawTriangle3D( [1.0,0.0,0.0, 1.0,0.0,1.0, 1.0,1.0,1.0 ]);
+
+        // Back Face z = 1.0
+        drawTriangle3D( [0.0,0.0,1.0, 0.0,1.0,1.0, 1.0,1.0,1.0 ]);
+        drawTriangle3D( [0.0,0.0,1.0, 1.0,0.0,1.0, 1.0,1.0,1.0 ]);
+    }
+}
